bbe6c4d9a690d58f794c3ef90dbe584133a4808a
chiomaT <chiomanwabugwu11@yahoo.com>
2022-05-18 23:35:41 +0100

    transaction

---
+++ lightpay/src/pages/Transactions/Transaction.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Transaction.css";
+
+const Transaction = () => {
+  return (
+    <>
+      <div className="contain">
+        <div className="main">
+          <h3>Hello world</h3>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Transaction;
