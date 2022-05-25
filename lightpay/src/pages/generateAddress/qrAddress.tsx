1ed02bb016da2fa02d6cfc6e90b470bfe813b2a6
durotemy <durotemy@gmail.com>
2022-05-25 18:20:29 +0100

    changes to qrCode

--- lightpay/src/pages/generateAddress/qrAddress.tsx
+++ lightpay/src/pages/generateAddress/qrAddress.tsx
@@ -1,47 +1,95 @@
-import "./qrAddress.css";
-import React, { useState } from "react"
+import "./qrAddress.module.css";
+import styles from "./qrAddress.module.css"
+
+import React, { useEffect, useState } from "react";
 import QRcode from "qrcode.react";
+import axios, { AxiosRequestConfig } from "axios";
 import { IoCopyOutline } from "react-icons/io5";
 import { IoRefresh } from "react-icons/io5";
 import { IoCloseOutline } from "react-icons/io5";
 
-
 function GenerateQr() {
   const [qrValue, setQrValue] = useState([] as any);
+
   const handleOnChange = (event: any) => {
     const { value } = event.target;
     setQrValue(value);
   };
+
+  const handleQrDisplay = () => {
+    // Add a request interceptor
+    let token = (localStorage.getItem('userToken')) as string;
+    token = JSON.parse(token)
+    axios.interceptors.request.use(function (config: AxiosRequestConfig) {
+
+      if (config.headers === undefined) {
+        config.headers = {};
+      }
+
+      config.headers.Authorization = token ? `Bearer ${token}` : '';
+      // config.headers.Authorization = `Bearer ${token}`;
+
+      return config;
+    });
+
+    console.log(token);
+
+    axios.get(`http://localhost:3001/userwallet`)
+      .then((response) => {
+        console.log(response.data);
+        setQrValue(response.data[0].address);
+      })
+      .catch(function (error) {
+        if (error.response) {
+          // The request was made and the server responded with a status code
+          // that falls out of the range of 2xx
+          console.log(error.response.data);
+          // setMessage(error.response.data.msg);
+        } else if (error.request) {
+          // The request was made but no response was received
+          console.log(error.request);
+          // setMessage(error.request);
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          console.log("Error", error.message);
+          // setMessage("Error" + error.message);
+        }
+      });
+  };
+
+  useEffect(() => {
+    handleQrDisplay();
+  }, []);
+
   return (
-    <div className="container">
-      <div className="App">
-        <div className="topIcons">
-          <IoCloseOutline />
-          <div className="refreshIcon">
-            <IoRefresh />
-            <p>Refresh</p>
+    <div className={styles.main_container}>
+      <div className={styles.container}>
+        <div className={styles.App}>
+          <div className={styles.topIcons}>
+            <IoCloseOutline />
+            <div className={styles.refreshIcon}>
+              <IoRefresh />
+              <p className={styles.para}>Refresh</p>
+            </div>
+          </div>
+          <h3 className={styles.text}><strong>Wallet Address</strong></h3>
+          <div className={styles.inputContainer}>
+            <IoCopyOutline className={styles.icon} />
+            <div className={styles.input}> <input onChange={handleOnChange} value={qrValue} className={styles.icon} /></div>
+          </div>
+          <div className={styles.qrContains}>
+            <QRcode
+              id="qrgen"
+              value={qrValue}
+              level={"H"}
+              includeMargin={false}
+              size={200}
+              className={styles.qrContainer} />
           </div>
         </div>
-        <h3 className="text">Wallet Address</h3>
-        <div className="inputContainer">
-          <IoCopyOutline className="icon" />
-          <input
-            onChange={handleOnChange} className="icon" />
-        </div>
-        <div className="grs" >
-          <QRcode
-            id="qr-gen"
-            value={qrValue}
-            level={"H"}
-            includeMargin={false}
-            size={90}
-            className="qrContainer"
-          />
-        </div>
-      </div>
-    </div >
+    </div>
+    </div>
   );
 }
 
 export default GenerateQr;
-
